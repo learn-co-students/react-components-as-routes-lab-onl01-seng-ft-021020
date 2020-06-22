694b5cdcc1acfd64c741a6a87be8b362b8e4b54e
catwhitmer <catwhitmer@gmail.com>
2020-06-22 08:37:10 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const render = actors.map((actor) => {
+    return <div>Name{actor.name} Movies:{actor.movies}</div>
+  });
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {render}
     </div>
   );
 };
